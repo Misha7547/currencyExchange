a685d7b5e5837cb531628d73d3c7f0f13f82e4fb
Misha Erin <misha7547.8363@gmail.com>
2024-03-22 01:30:40 +0700

    currencyExchange  - gloss

--- src/main/java/ru/skillbox/currency/exchange/entity/CurrencyXml.java
+++ src/main/java/ru/skillbox/currency/exchange/entity/CurrencyXml.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 
-import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
@@ -14,30 +13,7 @@ import java.util.List;
 @XmlRootElement(name = "ValCurs")
 public class CurrencyXml {
 
-    private String date;
-
-    private String name;
-
     private List<CurrencyDetailsXml> valutes;
-
-
-    @XmlAttribute(name = "Date")
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    @XmlAttribute(name = "name")
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @XmlElement(name = "Valute")
     public List<CurrencyDetailsXml> getValutes() {
